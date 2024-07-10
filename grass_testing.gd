a04ccb49214898d2b320f0e12160015a2b6c7725
Seventyy <seventyyyyy@gmail.com>
2024-07-11 01:50:36 +0200

    sadsd

--- grass_testing.gd
+++ grass_testing.gd
@@ -4,16 +4,16 @@ extends MultiMeshInstance2D
 @export var populate_button:bool:
 	set(val):
 		populate_basic()
+@export var box_size:Vector2i
+@export var tile_size:Vector2 = Vector2(32,32)
 
-var positions:Array[Vector2]
-var tile_size:Vector2 = Vector2(32,32)
+var positions:Array[Vector2i]
 
 func populate_basic() -> void:
-	tile_size = Vector2(32,32)
 	positions.clear()
-	for i in 10:
-		for j in 10:
-			positions.append(Vector2(i,j) * tile_size)
+	for x in box_size.x:
+		for y in box_size.y:
+			positions.append(Vector2i(x,y))
 	
 	multimesh.instance_count = positions.size()
 	
@@ -21,8 +21,7 @@ func populate_basic() -> void:
 		var grass_bunch_transform:Transform2D
 		grass_bunch_transform = grass_bunch_transform \
 			.translated(
-				(Vector2(positions[i]) + Vector2(.5,.5)) *
-				tile_size.x
+				(Vector2(positions[i]) + Vector2(.5,.5)) * tile_size.x
 			)\
 			.rotated_local(PI)\
 			.scaled_local(tile_size)
