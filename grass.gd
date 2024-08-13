5de9abaeb587b287163345f6c9a58e4a15299b32
Seventyy <seventyyyyy@gmail.com>
2024-08-13 17:50:22 +0200

    shader wygląda chyba git

--- grass.gd
+++ grass.gd
@@ -1,4 +1,6 @@
+@tool
 extends MultiMeshInstance2D
+
 
 @export var tilemap:TileMap
 
@@ -9,16 +11,50 @@ extends MultiMeshInstance2D
 @onready var positions:Array[Vector2i] = tilemap.get_used_cells(0)
 @onready var tile_size:Vector2 = tilemap.tile_set.tile_size 
 
+@export var trigger_ready:bool:
+	set(val):
+		_ready()
+
+@export var spawn_single:bool:
+	set(val):
+		multimesh.instance_count = 1
+		
+		var grass_bunch_transform:Transform2D
+		grass_bunch_transform = grass_bunch_transform \
+			.translated(Vector2.ONE * .5) \
+			.rotated_local(PI) \
+			.scaled_local(Vector2.ONE * 32)
+		multimesh.set_instance_transform_2d(0, grass_bunch_transform)
+
+
+
 func _ready() -> void:
 	positions.append(Vector2i(0, 0))
-	positions.append(Vector2i(1, 0))
-	positions.append(Vector2i(0, 1))
-	positions.append(Vector2i(1, 1))
-	populate_basic()
+	populate_test_rectangle()
 	#populate_double()
 	#populate_with_scale_variation()
 	#populate_with_scale_variation_and_density_multiplier(5)
+
+func populate_test_rectangle(size:Vector2i = Vector2i(50, 50)):
+	positions.clear()
 	
+	for x in size.x:
+		for y in size.y:
+			positions.append(Vector2i(x, y))
+	
+	multimesh.instance_count = positions.size()
+	
+	for i in positions.size():
+		var grass_bunch_transform:Transform2D
+		grass_bunch_transform = grass_bunch_transform \
+			.translated(
+				(Vector2(positions[i]) + Vector2(.5,.5)) *
+				tile_size.x
+			)\
+			.rotated_local(PI)\
+			.scaled_local(tile_size)
+		multimesh.set_instance_transform_2d(i, grass_bunch_transform)
+
 func populate_with_scale_variation_and_density_multiplier(amount_per_tile:int = 1):
 	multimesh.instance_count = positions.size()
 	
